d8ce776a8b80c191d3f17c3659c57424b5c8f784
Mike Maether <mike.maether@gmail.com>
2019-12-26 12:35:14 -0500

    Changing AMI type

--- example.tf
+++ example.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   profile = "default"
-  #region  = "us-east-1"
   region  = "us-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
+  #ami           = "ami-b374d5a5"
+  ami           = "ami-00068cd7555f543d5"
   instance_type = "t2.micro"
 }
