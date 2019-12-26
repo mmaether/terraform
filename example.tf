012cdb75ff727d6f9ebf0da5f9dea3f2fb31666b
Mike Maether <mike.maether@gmail.com>
2019-12-26 13:21:40 -0500

    Changing AMI type

--- example.tf
+++ example.tf
@@ -4,7 +4,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
-  #ami           = "ami-00068cd7555f543d5"
+  #ami           = "ami-b374d5a5"
+  ami           = "ami-00068cd7555f543d5"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "Demo mmaether instance"
+  }
 }
